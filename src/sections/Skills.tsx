1839c15531f9d329a97108082c26fc4beff47d1a
perez <perezalexandre430@gmail.com>
2026-01-26 10:03:39 +0100

    Skills begin

---
+++ src/sections/Skills.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import BtnContact from "../components/utils/BtnContact";
+import BtnGhost from "../components/utils/BtnGhost";
+import BackgroundParticles from "../components/utils/BackgroundParticles";
+import "Skills.scss";
+
+function Skills () {
+    return (
+        <p>Salut</p>
+    );
+}
+
+export default Skills;
