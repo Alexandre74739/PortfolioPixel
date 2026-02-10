cfe6f185cc2d1dcf487268a02fc4cb9544719214
perez <perezalexandre430@gmail.com>
2026-02-10 21:10:10 +0100

    chore: stop tracking .env and .history

--- .history/src/sections/HeroArcades_20260129144641.tsx
+++
@@ -1,7 +0,0 @@
-refais sur thème clair, plus simple et avec un système de jeux 
-
-en terme de contenue je veux un titre, un sous titre <p> et ces items import BtnContact from "../components/utils/BtnContact";
-
-import BtnGhost from "../components/utils/BtnGhost";
-
-import BackgroundParticles from "../components/utils/BackgroundParticles";
